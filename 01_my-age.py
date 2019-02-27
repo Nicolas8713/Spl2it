558a8a69f317cbccfc295c70fe23ab8ace9db575
Nicolas Hassler <171wita09@o365.htl-leoben.at>
2019-02-27 14:42:36 +0100

    ceated 01_my-age.py

--- 01_my-age.py
+++ 01_my-age.py
@@ -4,4 +4,4 @@ name = input("Wer bist du")
 alter=input ("Wie alt bist du?")
 alter = int(alter)
 alter2= alter + 1
-print = ( name , "Nächstes Jahr bist du:", alter2,"alt")
+print( name , "Nächstes Jahr bist du:", alter2,"alt")
