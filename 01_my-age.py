076b39f35e250dda4333659687a47920843c202d
Nicolas Hassler <171wita09@o365.htl-leoben.at>
2019-02-27 14:36:51 +0100

    ceated 01_my-age.py

--- 01_my-age.py
+++ 01_my-age.py
@@ -1,7 +1,7 @@
 #01_my-age.py
 
 name = input("Wer bist du")
-alter=("Wie alt bist du?)
+alter=input ("Wie alt bist du?")
 alter = int(alter)
 alter2= alter + 1
 print = ( name , "Nächstes Jahr bist du:", alter2,"alt")
--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
 # helloworld.py
 
 print("hello world")
+print ("Hello World" , "Internet","Schule")
+print ("Hello World" + "Internet"+"Schule")
