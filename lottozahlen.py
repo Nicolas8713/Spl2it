aeaa54d7a279f2789727e605c22d137f14d96a84
Nicolas Hassler <171wita09@o365.htl-leoben.at>
2019-03-06 15:01:03 +0100

    lottozahlen2.py

--- lottozahlen.py
+++ lottozahlen.py
@@ -4,8 +4,12 @@ import random
 random.seed()
 lottozahlen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45]
 lottogezogen=[]
-for i in range (0,6):
-	ziehung = random.randint(0,45)
-	lottogezogen.add(lottozahlen(ziehung))
-	lottozahlen.remove(lottozahlen(ziehung))
+for i in range (1,7):
+	i = random.randint(0, len(lottozahlen)-1)
+	x = lottozahlen.pop(i)
+	lottogezogen.append(x)
+
+
+	
+
 print(lottogezogen)
---
+++ lottozahlen2.py
@@ -0,0 +1,27 @@
+#lottozahlen2.py
+
+import random
+random.seed()
+tipp =("Bitte geben sie die Zahlen ein")
+tipps =tipp.split(", ")
+tipps =[int(i) for i in tipps]
+tipps.sort()
+lottogezogen=[]
+while tipps!=lottogezogen:
+	lottogezogen=[]
+	lottozahlen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45]
+	for i in range (1,7):
+		i = random.randint(0, len(lottozahlen)-1)
+		x = lottozahlen.pop(i)
+		lottogezogen.append(x)
+	lottogezogen.sort()
+	print (lottogezogen)
+	if (lottogezogen!=tipps):
+		counter=counter+1
+	if(lottogezogen==tipps):
+		print("Es hat",counter,"durchläufe gebraucht")
+		break
+
+	
+
+print(lottogezogen)
